9d4c91df0122027374daf7041fd0b4483f5ff38b
Edvin DAVULCU <edvindavulcu@gmail.com>
2021-02-03 00:58:20 +0300

    Kamp Odev

---
+++ Homeworks/Fibonacci.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Homeworks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int sayi1 = 0;
+            int sayi2 = 1;
+            Console.WriteLine("0");
+            Console.WriteLine("1");
+
+            for (int i = 0; i < 10; i++)
+            {
+                int toplam = sayi1 + sayi2;
+
+                Console.WriteLine(toplam);
+                sayi1 = sayi2;
+                sayi2 = toplam;
+            }
+
+        }
+
+        
+    }
+    
+    
+}
